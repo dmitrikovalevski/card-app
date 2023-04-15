e7a62c9e71753ba7f0bee9e3b2f0a676b8cf10f5
Dzmitry Kavaleuski <dzmitry.kavaleuski@sqilsoft.by>
2023-04-15 11:30:41 +0300

    remove user service from card module

--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -4,12 +4,11 @@ import { Card } from 'src/entity/card';
 import { CardService } from './card.service';
 import { CardController } from './card.controller';
 import { User } from 'src/entity/user';
-import { UserService } from 'src/user/user.service';
 
 
 @Module({
   imports: [TypeOrmModule.forFeature([Card, User])],
-  providers: [CardService, UserService],
+  providers: [CardService],
   controllers: [CardController],
   exports: [TypeOrmModule]
 })
